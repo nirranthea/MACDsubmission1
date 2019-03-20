b7cc4825a17083deda3081f336883384e14a299d
zidi nirranthea <nirranthea@gmail.com>
2019-03-20 21:41:17 +0700

    insert server user db info into code

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "nirrantheawebappserver.database.windows.net";
+    $user = "nirranthea";
+    $pass = "Purnama101";
+    $db = "dbo.Registration";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
